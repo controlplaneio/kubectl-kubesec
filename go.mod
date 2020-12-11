d5cf5f2ae82bb15843e5b6f94135ddf335c70fa8
06kellyjac <jack@control-plane.io>
2020-12-11 13:37:19 +0000

    refactor: refactor out legacy InitClientAndConfig

--- go.mod
+++ go.mod
@@ -3,11 +3,7 @@ module github.com/controlplaneio/kubectl-kubesec/v2
 go 1.15
 
 require (
-	github.com/google/gofuzz v1.2.0 // indirect
-	github.com/spf13/cobra v1.0.0
-	github.com/stretchr/testify v1.6.1 // indirect
-	golang.org/x/net v0.0.0-20201202161906-c7110b5ffcbb // indirect
-	gopkg.in/yaml.v2 v2.3.0 // indirect
-	k8s.io/apimachinery v0.19.4
-	k8s.io/client-go v0.19.4
+	github.com/spf13/cobra v1.1.1
+	k8s.io/apimachinery v0.20.0
+	k8s.io/client-go v0.20.0
 )
--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -1,12 +1,14 @@
 package cmd
 
 import (
+	"io/ioutil"
+	"os"
+
 	"github.com/spf13/cobra"
 	kjson "k8s.io/apimachinery/pkg/runtime/serializer/json"
 	"k8s.io/client-go/kubernetes"
-
 	"k8s.io/client-go/kubernetes/scheme"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 var RootCmd = &cobra.Command{
@@ -39,10 +41,32 @@ func init() {
 }
 
 func loadConfig() *kubernetes.Clientset {
-	restConfig, _, err := pluginutils.InitClientAndConfig()
+	home := os.Getenv("HOME")
+	if len(home) == 0 {
+		panic("no home")
+	}
+	clientSet, err := clientSet(home + "/.kube/config")
 	if err != nil {
 		panic(err)
 	}
-	c := kubernetes.NewForConfigOrDie(restConfig)
-	return c
+	return clientSet
+}
+
+func clientSet(kubeconfig string) (*kubernetes.Clientset, error) {
+	kubeconfigBytes, err := ioutil.ReadFile(kubeconfig)
+	if err != nil {
+		return nil, err
+	}
+
+	restConfig, err := clientcmd.RESTConfigFromKubeConfig(kubeconfigBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	clientset, err := kubernetes.NewForConfig(restConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return clientset, nil
 }
